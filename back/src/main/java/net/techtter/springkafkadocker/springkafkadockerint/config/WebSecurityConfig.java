42be701783013e84d27a262be6a2d4553c65c61b
Marcos Diógenes <marcosdiogenesjr@gmail.com>
2021-03-08 01:45:58 -0300

    impl swagger

---
+++ back/src/main/java/net/techtter/springkafkadocker/springkafkadockerint/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package net.techtter.springkafkadocker.springkafkadockerint.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{bcrypt}$2a$10$6CW1agMzVzBhxDzK0PcxrO/cQcmN9h8ZriVEPy.6DJbVeyATG5mWe")
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http
+               .authorizeRequests()
+               //.anyRequest().authenticated()
+                .antMatchers("/v2/api-docs").authenticated()
+                .and()
+                .httpBasic();
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.POST, "/user/login");
+        web.ignoring().antMatchers(HttpMethod.GET, "/consultorias");
+        web.ignoring().antMatchers(HttpMethod.GET, "/normas");
+        web.ignoring().antMatchers(HttpMethod.GET, "/processo");
+        web.ignoring().antMatchers(HttpMethod.POST, "/processo/agendar-etapa-processo");
+        /*web.ignoring().antMatchers("/v2/api-docs/**");
+        web.ignoring().antMatchers("/swagger.json");
+        web.ignoring().antMatchers("/swagger-ui.html");
+        web.ignoring().antMatchers("/swagger-resources/**");
+        web.ignoring().antMatchers("/webjars/**");*/
+    }
+}
