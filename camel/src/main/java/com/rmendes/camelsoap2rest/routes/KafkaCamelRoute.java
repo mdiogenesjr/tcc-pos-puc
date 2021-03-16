73d413cdfc07ffee609b8c43a7e08a8ce1d8b0ad
Marcos Diógenes <marcosdiogenesjr@gmail.com>
2021-03-16 13:34:38 -0300

    impl camel-kafka

---
+++ camel/src/main/java/com/rmendes/camelsoap2rest/routes/KafkaCamelRoute.java
@@ -0,0 +1,27 @@
+package com.rmendes.camelsoap2rest.routes;
+
+import com.rmendes.camelsoap2rest.bean.KafkaOutputBean;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaCamelRoute {
+
+	@Bean(name = "KafkaRouteProducer")
+	public RouteBuilder kafkaRouteProducer() {
+		return new KafkaRouteProducer(
+				"kafka:agendar-etapa-processo?brokers=localhost:9092&groupId=my_group_id&autoOffsetReset=earliest&consumersCount=1");
+	}
+
+
+	@Bean(name = "KafkaRouteConsumer")
+	public RouteBuilder kafkaRouteConsumer() {
+		return new RouteBuilder() {
+			public void configure() {
+				from("kafka:agendar-etapa-processo?brokers=localhost:9092&groupId=my_group_id&autoOffsetReset=earliest&consumersCount=1")
+				.bean(KafkaOutputBean.class, "doWork");
+			}
+		};
+	}
+}
